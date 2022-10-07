dc26711e18331a4c40d848263f3a156080985900
Aleksey-Sartakov <sartakov.aleksey.d@gmail.com>
2022-10-07 17:10:05 +0700

    added print_progress function

--- Лаб_1_Python/main.py
+++ Лаб_1_Python/main.py
@@ -1,9 +1,13 @@
 import urllib3
-from time import time
+import time
 import sys
+from threading import Thread
 
 
 CHUNK_SIZE = 1024
+
+downloaded_size: int = 0
+file_size: float = 0
 
 
 def convert_to_Mbytes(size: int) -> float:
@@ -16,26 +20,34 @@ def convert_to_Mbytes(size: int) -> float:
 
 
 def download_file(url: str):
+    global downloaded_size
+    global file_size
+
     url_connection = urllib3.PoolManager()
     response = url_connection.request("GET", url, preload_content=False)
 
     file_name = url.split("/")[-1]
     file_size = convert_to_Mbytes(int(response.getheader("Content-Length")))
-    downloaded_size = 0
 
-    prev_time = time()
     with open(file_name, "wb") as file:
         while chunk := response.read(CHUNK_SIZE):
             file.write(chunk)
 
             downloaded_size += CHUNK_SIZE
 
-            if (cur_time := time()) - prev_time > 1.0:
-                print(f"alredy downloaded.........[{convert_to_Mbytes(downloaded_size)}MB/{file_size}MB]")
-                prev_time = cur_time
 
-        print(f"Downloaded successfully...[{file_size}MB/{file_size}MB]")
+def print_progress():
+    while True:
+        time.sleep(1)
+        print(f"alredy downloaded.........[{convert_to_Mbytes(downloaded_size)}MB/{file_size}MB]")
+
+        if convert_to_Mbytes(downloaded_size) >= file_size:
+            print(f"Downloaded successfully...[{file_size}MB/{file_size}MB]")
+            break
 
 
 if __name__ == "__main__":
+    thread = Thread(target=print_progress)
+    thread.start()
+
     download_file(sys.argv[1])
